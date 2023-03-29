e7c1838cb0c2d938a3c44acea0e70fe1c0539bc3
Alex Perez <alejandro.gperez745@gmail.com>
2023-03-29 12:05:48 -0500

    more env

--- week7/day3/assignment/static/client.js
+++ week7/day3/assignment/static/client.js
@@ -5,24 +5,22 @@ const chats = document.querySelector("#chat")
 const userCount = document.querySelector("#userCount")
 
 button.addEventListener("click", () => {
-  const username = user.value
-  const message = chatMessage.value
-  socket.emit("JavaScript", { username: username, message: message })
+    const username = user.value
+    const message = chatMessage.value
+    socket.emit("JavaScript", { username: username, message: message })
 })
 
 socket.on("JavaScript-joined", (chatMessages) => {
-  const chat = chatMessages.map((message) => {
-    return `<li>${message.username}: ${message.message}</li>`
-  })
-  chats.innerHTML = chat.join("")
+    const chat = chatMessages.map((message) => {
+        return `<li>${message.username}: ${message.message}</li>`
+    })
+    chats.innerHTML = chat.join("")
 
-  //   const count = io.engine.clientsCount
-  //   console.log(count)
-  const counts = `<div>Users in room: ${count}</div>`
-  userCount.innerHTML = counts
+    const counts = `<div>Users in room: ${count}</div>`
+    userCount.innerHTML = counts
 })
 
 socket.on("JavaScript", (chat) => {
-  const newMessage = `<li>${chat.username}: ${chat.message}</li>`
-  chats.insertAdjacentHTML("beforeend", newMessage)
+    const newMessage = `<li>${chat.username}: ${chat.message}</li>`
+    chats.insertAdjacentHTML("beforeend", newMessage)
 })
