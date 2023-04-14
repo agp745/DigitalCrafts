f059294f583b4bd76cca2fe99968fdb643bf4a55
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-04-14 13:51:35 -0500

    day5 update

--- week10/day3/assignment/server/routes/authRouter.js
+++ week10/day3/assignment/server/routes/authRouter.js
@@ -20,27 +20,29 @@ router.post("/auth/signup", async (req, res) => {
 })
 
 router.post("/auth/login", async (req, res) => {
-    const username = req.body.username
     const password = req.body.password
 
-    const user = models.User.findOne({
+    const user = await models.User.findOne({
         where: {
-            username: username
+            username: req.body.username
         }
     })
 
+    console.log(user)
+
+    bcrypt.decodeBase64
 
     if(user) {
-        const result = await bcrypt.compare(password, user.password)
-
+        const result = await bcrypt.compare(password, user.dataValues.password)
+        console.log(result)
         if(result) {
-            res.json({"success": "logged in"})
+            res.status(200).json({"success": "user logged in"})
         } else {
-            res.json({"error": "bad credentials"})
+            res.status(400).json({"error": "bad credentials"})
         }
         
     } else {
-        res.json({"error": "bad credentials"})
+        res.status(400).json({"error": "bad credentials"})
     }
 })
 
